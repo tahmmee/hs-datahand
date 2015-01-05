4aaca06427bb2e5eb1a6f8649666ae57019db472
Eric Litak <elitak@gmail.com>
2015-01-05 04:49:29 -0800

    cabal configure too

--- build+program.sh
+++ build+program.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dhteensy="../dhteensy"
-cabal build && dist/build/hs-datahand/hs-datahand | tee "${dhteensy}/keymaps-elitak.h"
+cabal configure && cabal build && dist/build/hs-datahand/hs-datahand | tee "${dhteensy}/keymaps-elitak.h"
 cd "${dhteensy}" ; make ; cd -
 teensy_loader_cli -mmcu=at90usb1286 -w "${dhteensy}/dhteensy.hex"
