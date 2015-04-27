bfd48da0ee62d687d112ff72615ae86e6c82f1c7
Eric Litak <elitak@gmail.com>
2015-04-27 11:08:03 -0700

    attempts to get any sign of life

--- stm32f10x/hs_src/Main.hs
+++ stm32f10x/hs_src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 module Main where
 import Foreign.Ptr
 import Foreign.Storable
@@ -8,15 +9,19 @@ import Data.List (intersperse)
 import DataHand.Layout
 import DataHand.Layouts.ProgrammerDvorak
 import DataHand.USB.Descriptors
+import Data.Bits
 
 foreign import ccall "c_extern.h Delay" c_delay :: Word32 -> IO ()
+#include "stm32f10x.h"
 
 gpioPtr :: Ptr Word32
+-- TODO: import these as defines via ffi
+-- these are all #defined in lib/CMSIS/Core/CM3/stm32f10x.h
 gpioPtr = odr
   where periphBase     = nullPtr        `plusPtr` 0x40000000
-        arb2periphBase = periphBase     `plusPtr` 0x10000
-        gpioaBase      = arb2periphBase `plusPtr` 0x0800
-        odr            = gpioaBase      `plusPtr` 12
+        apb2periphBase = periphBase     `plusPtr` 0x10000
+        gpioaBase      = apb2periphBase `plusPtr` 0x0800 -- each gpio port addr points to a struct GPIO_TypeDef
+        odr            = gpioaBase      `plusPtr` 12 -- Output Data Register pins (4th uint32 in the struct)
 
 gpioOut :: Word32 -> IO ()
 gpioOut v = do
@@ -24,38 +29,15 @@ gpioOut v = do
   c_delay 500000
 
 join delim l = concat (intersperse delim l)
+
 main :: IO ()
 main = do
     let layerToRawMap EmptyLayer = []
         layerToRawMap l = map fromEnum $ (toList . toRaw) l
---    let dumpRawHeader Layout{normal=normal, nas=nas, function=function} = join "\n" [
---            "// This header was generated using https://github.com/elitak/hs-datahand"
---          , "const char PROGMEM normal_keys [] = {" ++ join ", " (map show $ layerToRawMap normal  ) ++ "};"
---          , "const char PROGMEM    nas_keys [] = {" ++ join ", " (map show $ layerToRawMap nas     ) ++ "};"
---          , "const char PROGMEM     fn_keys [] = {" ++ join ", " (map show $ layerToRawMap function) ++ "};"
---            ]
-    --hdr <- return $ dumpRawHeader experimental
     return $ layerToRawMap (normal experimental)
-    return ()
---main = forever $ mapM_ gpioOut pat
---  where
---    o13 = (1 `shiftL` 13)
---    o15 = (1 `shiftL` 15)
---    pat = [o13, o13 .|. o15, o15, 0]
-
-{--
-lib/CMSIS/Core/CM3/stm32f10x.h
-define PERIPH_BASE           ((uint32_t)0x40000000) /*!< SRAM base address in
-define APB2PERIPH_BASE       (PERIPH_BASE + 0x10000)
-define GPIOA_BASE            (APB2PERIPH_BASE + 0x0800)
-typedef struct
-{
-  __IO uint32_t CRL;
-  __IO uint32_t CRH;
-  __IO uint32_t IDR;
-  __IO uint32_t ODR;
-  __IO uint32_t BSRR;
-  __IO uint32_t BRR;
-  __IO uint32_t LCKR;
-} GPIO_TypeDef;
---}
+    forever $ gpioOut 0xff
+    --forever $ mapM_ gpioOut pat
+    --where
+    --  o13 = (1 `shiftL` 0)
+    --  o15 = (1 `shiftL` 1)
+    --  pat = [o13, o13 .|. o15, o15, 0]
